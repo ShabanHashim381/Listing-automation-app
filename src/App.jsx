39416822d902edf019c6ec181d2112d494275dc2
Shaban Hashim <shabantararr381@gmail.com>
2025-11-05 05:19:30 +0500

    ThemeToaster Bug Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import AppRoutes from "./Routes/Index";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeToaster } from "./components/UI/ThemeToaster";
 import { AuthProvider } from "./Context/AuthContext";
+import { ThemeToaster } from "./components/UI/ThemeToaster";
 
 function App() {
   return (
